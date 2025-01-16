35858e24e1c97d40796e6d677ef3fdfd75f2b56d
Anav Chand <anavkki@gmail.com>
2025-01-16 20:44:10 +1200

    fix: correct colorscheme borders

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,7 +1,11 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+    "ellisonleao/gruvbox.nvim",
 
-	config = function()
-		vim.cmd.colorscheme "gruvbox"
-	end,
+    config = function()
+        vim.cmd.colorscheme "gruvbox"
+        vim.api.nvim_set_hl(0, "NormalFloat", {
+            fg = "none",
+            bg = "none",
+        }) 
+    end,
 }

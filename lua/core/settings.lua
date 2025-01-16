e1c58f0fe0baf9189d47c2349ccbb083f663e5f8
Anav Chand <anavkki@gmail.com>
2025-01-16 20:43:23 +1200

    feat: add vim settings

---
+++ lua/core/settings.lua
@@ -0,0 +1,17 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.fillchars = { eob = " "}
+
+vim.g.lazygit_floating_window_scaling_factor = 0.8

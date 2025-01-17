06e6e235dc7928ca60d13993d0310fe91d86aeb2
Anav Chand <anavkki@gmail.com>
2025-01-17 22:09:07 +1200

    chore: clean up borders and window sizes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,6 @@
 return {
-    "akinsho/toggleterm.nvim", 
-    version = "*", 
+    "akinsho/toggleterm.nvim",
+    version = "*",
     opts = {
         open_mapping = [[<F12>]],
         insert_mappings = true,
@@ -9,7 +9,13 @@ return {
         close_on_exit = true,
         direction = "float",
         float_opts = {
-            border = "curved"
+            border = "curved",
+            width = function()
+                return math.ceil(vim.o.columns * 0.8)
+            end,
+            height = function()
+                return math.ceil(vim.o.lines * 0.8)
+            end,
         }
     }
 }

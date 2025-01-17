7bc0e51b766bd7baf9fcf0007f9103228b4ae4b0
Anav Chand <anavkki@gmail.com>
2025-01-17 22:24:39 +1200

    chore: add more lsps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,7 +6,7 @@ return {
                 'williamboman/mason.nvim', config = true
             },
             'williamboman/mason-lspconfig.nvim',
-            'hrsh7th/cmp-nvim-lsp', 
+            'hrsh7th/cmp-nvim-lsp',
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
@@ -15,7 +15,7 @@ return {
             "saadparwaiz1/cmp_luasnip",
         },
         config = function()
-             local servers = { "ts_ls", "gopls", "jsonls", "cssls", "html", "astro", "prismals", "tailwindcss" }
+             local servers = { "bashls", "lua_ls", "vimls", "clangd", "ts_ls", "gopls", "jsonls", "cssls", "html", "tailwindcss" , "astro", "svelte", "prismals", "marksman", "dockerls", "docker_compose_language_service", "nginx_language_server", "ansiblels", "terraformls", "sqls", "emmet_ls", "eslint", "gitlab_ci_ls"}
 
             require('mason').setup({
                 ui = {

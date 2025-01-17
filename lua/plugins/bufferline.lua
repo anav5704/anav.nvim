d0a07879092d1e138bc3ef523b7fdc009da92cfc
Anav Chand <anavkki@gmail.com>
2025-01-17 15:30:50 +1200

    fix: add gruxbox as dep and increase priority

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,22 @@
 return {
     "akinsho/bufferline.nvim", 
+    priority = 1000,
     version = "*", 
-    dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "ellisonleao/gruvbox.nvim",
+    },
     config = function() 
-        require("bufferline").setup()
+        require("bufferline").setup({
+            options = {
+                show_buffer_close_icons = false,
+            },
+            highlights = {
+                buffer_selected = {
+                    bold = false,
+                    italic = false,
+                },
+            },
+        })
     end
 }

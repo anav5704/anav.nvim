840292a5c66443ebd480b95b8db01d337b2072ca
Anav Chand <anavkki@gmail.com>
2025-02-06 11:15:03 +1200

    style: remove italic strings

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,11 +1,13 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    "morhetz/gruvbox.nvim",
     priority = 1000,
     config = function()
         vim.cmd.colorscheme "gruvbox"
         vim.api.nvim_set_hl(0, "NormalFloat", {
             fg = "none",
             bg = "none",
-        }) 
+        })
+        vim.api.nvim_set_hl(0, "Comment", { italic = false })
+        vim.api.nvim_set_hl(0, "String", { italic = false })
     end,
 }

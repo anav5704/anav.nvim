d5f8adaca4f7c7d9ba741581760bd92676f90d73
Anav Chand <anavkki@gmail.com>
2025-02-10 16:35:36 +1200

    feat: add keybind to toggle copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,19 @@ return {
     event = "InsertEnter",
     config = function()
         vim.g.copilot_enabled = false
+
+        _G.ToggleCopilot = function()
+            local copilot_status = vim.g.copilot_enabled
+            if copilot_status then
+                vim.cmd("Copilot disable")
+                print("Copilot Disabled")
+            else
+                vim.cmd("Copilot enable")
+                print("Copilot Enabled")
+            end
+        end
+
+        vim.api.nvim_set_keymap("n", "<leader>c", ":lua ToggleCopilot()<CR>", { noremap = true, silent = true })
     end
 }
 

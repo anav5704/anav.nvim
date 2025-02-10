198a30ee38de08ad9b2e3ac39e9003ed5618624d
Anav Chand <anavkki@gmail.com>
2025-02-11 08:18:30 +1200

    chore: change  binding to toggle copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,14 +8,15 @@ return {
             local copilot_status = vim.g.copilot_enabled
             if copilot_status then
                 vim.cmd("Copilot disable")
+                vim.g.copilot_enabled = false
                 print("Copilot Disabled")
             else
                 vim.cmd("Copilot enable")
+                vim.g.copilot_enabled = true
                 print("Copilot Enabled")
             end
         end
 
-        vim.api.nvim_set_keymap("n", "<leader>c", ":lua ToggleCopilot()<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<F10>", ":lua ToggleCopilot()<CR>", { noremap = true, silent = true })
     end
 }
-
